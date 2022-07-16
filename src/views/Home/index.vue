cdb9fc82f4a33aa731d88feb68c786f8c869bad7
shijinbao1996 <1366221851@qq.com>
2022-07-16 17:02:52 +0800

    完成了我的页面

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,40 @@
 <template>
-  <div>主页</div>
+  <div>
+    <van-nav-bar class="navbar">
+      <template #title>
+        <van-button round>
+          <van-icon name="search"></van-icon>
+          搜索
+        </van-button>
+      </template>
+    </van-nav-bar>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style scoped lang="less">
+.navbar {
+  background-color: #3296fa;
+  color: #fff;
+  .van-button {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+  }
+  :deep(.van-nav-bar__title) {
+    max-width: unset;
+  }
+  .van-button__text {
+    color: #fff;
+  }
+  .van-icon-search {
+    color: #fff;
+  }
+  .van-button--default {
+    border: 0.02667rem solid #5babfb;
+  }
+}
+</style>
